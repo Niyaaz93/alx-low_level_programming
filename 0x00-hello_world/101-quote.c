efdc8278b359d597f4e1248b833e127d2f18a6a0
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-17 13:19:58 -0700

    Printing a string to the standard error without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 1 (Success)
  */
-int main()
+int main(void)
 {
-fwrite(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ,59);
 return (1);
 }
