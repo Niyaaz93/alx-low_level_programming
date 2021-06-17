04140311a8902d84d3df30d37d8fcc276b31d50d
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-17 09:20:40 -0700

    Printing a string using printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - it is the entry point
+ *        uses the printf function
+ *        to print a string
+ * Return: always 0 (successful)
+ */
+
+int main(void)
+{
+  printf("with proper grammer, but the outcome is a piece of art,");
+  return (0);
+}
