842e381aa182aed8a9b8d334067c0d7400f9dbf8
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-17 09:11:58 -0700

    Printing a string using the puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - is the entry point
- *        prints the string -> Programming is like building a multilingual puzzle
+ *        prints the string -> 
+ *        Programming is like building a multilingual puzzle
  *        followed by a new line
  * Return: always 0 (success)
  */
