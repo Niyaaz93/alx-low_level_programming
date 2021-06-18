683c3735a19d74fd41f95ea04590c2155b07ead5
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-18 08:46:52 -0700

    Printing all letters in lowercase besides q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - entry point to the function
+ * Description: print the alphabet in lowercase
+ * followed by a new line
+ * Return: always 0 (successful)
+ */
+
+int main(void)
+{
+int i;
+ char letters[26] = {'a', 'b', 'c', 'd', 'f', 'g', 'h',
+		     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+		     'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+for (i = 0; i < 26; i++)
+{
+putchar(letters[i]);
+}
+
+putchar('\n');
+
+return (0);
+}
