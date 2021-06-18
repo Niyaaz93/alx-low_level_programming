c76dd0ff75c24bee73a019263940f668abd1538c
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-18 11:41:13 -0700

    Print all possible combinations of 2 digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,9 +12,9 @@ int main(void)
 int first;
 int second;
 
-for (first = 0; first <= 9; first++)
+for (second = 0; second <= 9; second++)
 {
-for (second = first + 1; second <= 9; second++)
+for (first = second + 1; first <= 9; first++)
 {
 putchar(second + '0');
 putchar(first + '0');
