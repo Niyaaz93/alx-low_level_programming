215a72c9c8feda680409b3fed0064dba700a7bfe
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-18 06:39:21 -0700

    Using if_else statements to print statements depending on its output

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * main - the entry point
- * Description: using if and else conditional statements 
+ * Description: using if and else conditional statements
  * to test a few comparison statements
  * Return: always 0 (successful)
  */
@@ -14,17 +14,17 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n > 0)
+if (n > 0)
 {
-  printf("%d is positive\n", n);
+printf("%d is positive\n", n);
 }
-else if(n == 0)
+else if (n == 0)
 {
-  printf("%d is zero\n", n);
+printf("%d is zero\n", n);
 }
 else
 {
-  printf("%d is negative\n", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
