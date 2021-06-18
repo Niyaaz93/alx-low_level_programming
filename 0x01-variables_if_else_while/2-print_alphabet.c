54a8382a6a6b345c0df2a2da6d2c82dfae974d99
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-18 08:34:15 -0700

    Converts alphabet to lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,7 +11,7 @@ int main(void)
 {
 int i;
 char letters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-		     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+		     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q'
 		     'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
 for (i = 0; i < 26; i++)
