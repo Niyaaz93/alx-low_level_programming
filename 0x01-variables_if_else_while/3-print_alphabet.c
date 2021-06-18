f4b6fa53e8e477de808a2aa4b5558158ae054126
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-18 08:38:44 -0700

    Printing letters in lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - entry point to the function
+ * Description: print the alphabet in lowercase
+ * followed by a new line
+ * Return: always 0 (successful)
+ */
+
+int main(void)
+{
+int i;
+char letters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+		     'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+for (i = 0; i < 26; i++)
+{
+putchar(letters[i]);
+putchar(toupper(letters[i]);
+}
+
+putchar('\n');
+
+return (0);
+}
