bcd9991021c7c70cd20fb1927c92c15a39e3d68e
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-18 11:36:04 -0700

    Print all possible combinations of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry into the function
+ * Description: prints all possible combinations
+ * of 2 digit numbers
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+int first;
+int second;
+
+for (first = 0; first <= 9; first++)
+{
+for (second = first + 1; second <= 9; second++)
+{
+putchar(second + '0')
+putchar(first + '0')
+
+if (second == 8 && first == 9)
+{
+continue;
+}
+putchar(',');
+putchat(' ');
+}
+
+}
+
+putchar('\n');
+return (0);
+}
