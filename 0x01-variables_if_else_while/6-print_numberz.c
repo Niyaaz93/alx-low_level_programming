dd0422f4726bfe82c2f131de927a0ebfbbeb662a
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-18 09:56:05 -0700

    Printing all single digit numbers and using putchar function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point into the function
+ * Description: prints all single digit
+ * numbers of base 10
+ * Return: always 0 (successful)
+ */
+
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+{
+putchar(num);
+putchar('\n');
+}
+
+return (0);
+}
