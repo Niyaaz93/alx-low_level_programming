02857f6434e772e2795da18f2a51024760fad686
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-22 14:08:27 -0700

    Adding two integers together

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - function that adds to integers and returns the result
- * Description: Function accepts 2 integers and then returns the result of their
- * addition
+ * Description: Function accepts 2 integers and then returns
+ * the result of their addition
  * @a: first integer to be added
  * @b: second integer to be added
  * Return: result of integer addition
