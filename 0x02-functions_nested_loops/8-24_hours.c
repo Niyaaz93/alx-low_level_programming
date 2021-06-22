376506de92f3f8e857589d974fad57579f400bc8
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-22 12:22:10 -0700

    Printing every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * 24_hours - printing every minute of the day
+ * Description: Printing every minute of the day like
+ * in the Jack Bauer series.
+ * Return: void (successful)
+ */
+
+void jack_bauer(void)
+{
+	int hour;
+	int min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
