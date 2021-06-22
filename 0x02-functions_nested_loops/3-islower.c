18094392f16fc75fb8b317cda31b6c8abc902103
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-22 07:47:08 -0700

    Checking if a character is lowercase using ASCII code

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,20 +5,21 @@
  * Description: function checks for lowercase characters
  * and returns 1 if c character is lowercase
  * and returns 0 otherwise
- * @c - character in ASCII code
+ * @c: character in ASCII code
  * Return: 1 if c is lowercase, or 0 otherwise
  */
 
 int _islower(int c)
 {
 
-  if (c >= 97 && c <= 122)
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
-  _putchar('\n');
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+_putchar('\n');
 }
