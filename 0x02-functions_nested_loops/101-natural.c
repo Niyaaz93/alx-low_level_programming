0b41d4ac17a03a05a51bba8dab29214cf5d837bb
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-23 08:09:25 -0700

    Printing the sum of all natural numbers that are multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * natural - computing and printing the sum of
+ * main - computing and printing the sum of
  * all the multiples of 3 or 5 below 1024
  * Description: function that computes the and prints
  * the sums of all the multiples of 3 and 5 below 1024
@@ -25,7 +25,7 @@ int main(void)
 			sum = sum + i;
 		}
 	}
-	printf("%d", sum);
+	printf("%d\n", sum);
 
 	return (0);
 }
