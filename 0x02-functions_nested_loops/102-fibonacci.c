aa5f92a77f97713727412c63ce583be3bd653e95
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-23 08:30:29 -0700

    Printing the Fibonacci series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry into the function
+ * Description: Function prints the first 50 fibonacci numbers
+ * starting with 1 and 2, followed by a new line
+ * the numbers must be separated by a comma and a space
+ * Return: Always o (successful)
+*/
+
+int main(void)
+{
+	int i;
+	int term1 = 1;
+	int term2 = 2;
+	int n = 50;
+	int nexterm = term1 + term2;
+
+	for (i = 1; i < n; ++i)
+	{
+		printf("%d, ", nextterm);
+		term1 = term2;
+		term2 = nextterm;
+		nextterm = term1 + term2;
+	}
+	return (0);
+}
