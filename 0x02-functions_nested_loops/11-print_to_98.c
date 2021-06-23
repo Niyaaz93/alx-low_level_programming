c0529083d1a0f25cecf288f38efed9959d824a6c
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-23 03:15:35 -0700

    Printing from int n up to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,15 +16,15 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (i = n; n > 98 ; i--)
+		for (i = n; i > 98 ; i--)
 		{
 			printf("%d, ", i);
 		}
 	}
 
-	else
+	else if
 	{
-		for (i = n; n < 98; i++)
+		for (i = n; i < 98; i++)
 		{
 			printf("%d, ", i);
 		}
