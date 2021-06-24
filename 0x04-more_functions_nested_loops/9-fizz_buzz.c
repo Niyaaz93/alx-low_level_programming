b7c96ff45853c4ec85a139520b083a24884156c3
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-24 12:28:28 -0700

    Printing numbers from 1 to 100 and replace multiples of and 5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,15 +2,17 @@
 
 /**
  * main - entry point into the function
- *
- *
+ * Description: this function prints the numbers from 1 to 100
+ * followed by a newline and replace multiples of 3 and 5
+ * with the words Fizz and Buzz
+ * Return: Always 0 (successful)
  */
 
 int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (((i % 3) == 0) && ((i % 5) == 0))
 		{
