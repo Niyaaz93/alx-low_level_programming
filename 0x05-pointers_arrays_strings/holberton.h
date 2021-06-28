c147f0d1ab1ccea7039b22a9edd984c39b60797e
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-28 10:58:13 -0700

    Holberton header file

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,16 @@
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * reset_to_98 - updates the value of the paramter to 98
+ * @n - the pointer accepted by the function
+ *
+ * Return: void (successful)
+ */
+void reset_to_98(int *n);
