04495f83f0669d4d826ade83f928aefb3504a57a
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-28 11:08:49 -0700

    Swapping two integers around

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the valus of 2 integers
+ * @a - first integer to be swapped
+ * @b - second integer to be swapped
+ *
+ * Return: void (successful)
+ */
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
