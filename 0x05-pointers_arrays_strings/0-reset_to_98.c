8e73795262beadab42a7e376a65f7d348c58e402
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-28 11:03:29 -0700

    Changing the value of a variable using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - updates the value of the parameter to 98
+ * @n - pointer of type int accepted by function
+ *
+ * Return: void (successful)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98
+}
