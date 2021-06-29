179c1e8d31ef3b8e8a90e068281c77354357a29f
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-06-29 05:52:26 -0700

    Copying a string that is pointed to, to a buffer point

---
+++ 0x05-pointers_arrays_strings/strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - copies the string pointed to, to the buffer point
+ * @dest: buffer point
+ * @src: string to be copied
+ *
+ * Return: 0 (successful)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(src = i) != '\0'; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
