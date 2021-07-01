99220900170ce87c9c8140694734518907d7af79
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-07-01 07:58:36 -0700

    Capitilises all words in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalises all words of a string
+ * @str: string accepted by function
+ *
+ * Return: char
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 97 && str[i] <= 122)
+			&& (str[i - 1] == 44 || str[i - 1] == 59
+			    || str[i - 1] == 46 || str[i - 1] == 33
+			    || str[i - 1] == 63 || str[i - 1] == 34
+			    || str[i - 1] == 40 || str[i - 1] == 41
+			    || str[i - 1] == 123 || str[i - 1] == 125
+			    || str[i - 1] == 32 || str[i - 1] == '\n'
+			    || str[i - 1] == '\t'))
+			str[i] = str[i] - 97 + 65;
+		i++;
+	}
+
+	return (str);
+}
