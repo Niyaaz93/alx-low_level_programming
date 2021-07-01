82ccb4c54742c983972778564e2734a2743659c8
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-07-01 05:06:10 -0700

    Copying a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: the string returned by the function
+ * @src: the string that is copied from
+ * @n: number of characters copies from src
+ *
+ * Return: *char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len;
+
+	for (len = 0; len < n && src[len] != '\0'; len++)
+	{
+		dest[len] = src[len];
+	}
+
+	while (len < n)
+	{
+		dest[len] = '\0';
+
+		len++;
+	}
+
+	return (len);
+}
