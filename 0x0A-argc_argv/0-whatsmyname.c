6af678daa584c81d693cccecea60cd4aacdb916b
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-07-12 06:51:35 -0700

    Printing the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Prints the name of the program
+ * @argc: argument count containing number of args passed to program
+ * @argv: argument vector containing array of strings
+ *
+ * Return: 0 (successful)
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
