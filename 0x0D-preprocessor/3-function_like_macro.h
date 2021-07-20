476bef963e2b094fbd0502e75752c6de97dcaf76
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-07-20 03:16:37 -0700

    Function to determine the absolute value of a number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,5 @@
-#ifndef Function_LIKE_MACRO
+#ifndef FUNCTION_LIKE_MACRO
 #define FUNCTION_LIKE_MACRO
+
 #define ABS(x) ((x) < 0 ? (-1 * (x)) : (x))
-#endif /* ending ifndef ABS*/
+#endif /* ending ifndef FUNCTION_LIKE_MACRO*/
