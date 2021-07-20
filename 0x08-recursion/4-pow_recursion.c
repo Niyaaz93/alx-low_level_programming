c5ae8a7c32e34e2dbb996ad626bcde3ab6b19e8e
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-07-20 06:31:07 -0700

    Function that raises a number to a power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - function that returns x to power of y
+ * @x: number to be raised to a power
+ * @y: power that number is raised to
+ *
+ * Return: 0 on success, -1 on error
+ */
+int _pow_recursion(int x, int y)
+{
+	/* Base Case */
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if ( y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y-1));
+}
