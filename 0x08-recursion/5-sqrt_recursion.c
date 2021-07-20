2d193e33a148e0ae18d64586feca2e1ae4460439
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-07-20 08:06:26 -0700

    Function that returns sqrt of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+
+/**
+ * sqrt_test - function that test if for the sqrt of a num
+ * @num: number accpeted by function
+ * @mid: iterator
+ *
+ * Return: sqrt of the number given
+ */
+int sqrt_test(int low, int n)
+{
+	float mid;
+
+	mid = (n + low) / 2;
+
+	if ((mid * mid) == n)
+	{
+		return (mid);
+	}
+	else
+	{
+		if ((mid * mid) > n)
+		{
+			return (sqrt_test(low, mid));
+		}
+		else if ((n - low) < 0.001)
+		{
+			return (mid);
+		}
+		else
+		{
+			return (sqrt_test(mid, n));
+		}
+	}
+}
+
+
+/**
+ * _sqrt_recursion - function that return natural sqrt of a number
+ * @n: number accpeted by function
+ *
+ * Return: 0 on success, -1 on error
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_test(1, n));
+	}
+}
