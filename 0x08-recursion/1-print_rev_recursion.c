231fdd42972d51f9f5aea5193594fc2593648086
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-07-20 05:49:46 -0700

    Printing a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _print_rev_recursion - function that prints string in reverse
+ * @s: string accpeted by function
+ *
+ * Return: 0 on sucsess
+ */
+void _print_rev_recursion(char *s)
+{
+	while (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+
+		_putchar(s);
+	}
+}
