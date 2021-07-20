8c078ccb4298deecb6ec34291844c18d12ca48eb
Niyaaz93 <niyaaz.ismail93@gmail.com>
2021-07-20 08:47:21 -0700

    Function to check for primality

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - function that returns 1 for prime numbers
+ * and 0 otherwise
+ * @n: number accepted by function
+ *
+ * Return: 1 for prime number, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	int i;
+
+	i = n / 2;
+
+	if (n == 1)
+	{
+		return (1);
+	}
+	else if (n % i == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
